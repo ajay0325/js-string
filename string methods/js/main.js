d3e6a4cf0164055925e596ed30696ae4f399e51e
ajay0325 <m.ajay030398@gmail.com>
2022-09-23 10:03:59 +0530

    string

---
+++ string methods/js/main.js
@@ -0,0 +1,23 @@
+var a = 'Michael Ajay';
+var b = 'Muthu Balaji'
+var d = '  AJAY'
+
+var c = a.concat(b);
+console.log(c);
+
+console.log(a.length)
+console.log(b.startsWith('M'));
+console.log(b.endsWith('i'));
+console.log(a.includes('Ajay'));
+console.log(a.slice(0,8));
+console.log(b.replace('Balaji','Soosa'));
+console.log(a.toUpperCase());
+console.log(b.toLowerCase());
+console.log(a.indexOf('a'));
+console.log(a.lastIndexOf('a'));
+console.log(a.split(''));
+console.log(d.trim());
+console.log(a.charAt(4));
+console.log(a.repeat(6));
+console.log(a.charCodeAt(4));
+
